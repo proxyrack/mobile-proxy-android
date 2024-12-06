770f82c04def43407a33c1e49bcbcf79a474a11d
Paul Hendricks <paul@digitalhurricane.io>
2024-12-06 16:51:29 -0700

    fix(IPInfo): use different endpoint for fetching IP

--- app/src/main/java/com/proxyrack/control/data/network/IpInfoApiService.kt
+++ app/src/main/java/com/proxyrack/control/data/network/IpInfoApiService.kt
@@ -1,11 +1,9 @@
 package com.proxyrack.control.data.network
-import com.proxyrack.control.data.model.IpInfo
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Headers
 
 interface IpInfoApiService {
-    @Headers("Accept: application/json")
-    @GET("/")
-    fun getIpInfo(): Call<IpInfo>
+    @GET("/ip")
+    fun getIpInfo(): Call<ResponseBody>
 }
