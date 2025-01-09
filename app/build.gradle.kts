cf754668a623bc791e7102123f8971ffc751ef76
digitalhurricane-io <paul@digitalhurricane.io>
2025-01-09 10:24:16 -0700

    fix(clientType): change clientType to 'android' (#37)
    * fix(clientType): change clientType to 'android'
    
    * change github actions to use v4 of download-artifact and upload-artifact

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 21
         targetSdk = 34
         versionCode = 1
-        versionName = "1.3.7"
+        versionName = "1.3.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
