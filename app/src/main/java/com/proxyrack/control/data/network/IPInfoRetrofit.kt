fd8397812126750a3f86d2eb79c051ee22d28892
digitalhurricane-io <paul@digitalhurricane.io>
2025-01-28 10:20:07 -0800

    fix(okHtpp): default to no proxy for OkHttp library requests if the u… (#48)
    fix(okHtpp): default to no proxy for OkHttp library requests if the user is using invalid system proxy settings

--- app/src/main/java/com/proxyrack/control/data/network/IPInfoRetrofit.kt
+++ app/src/main/java/com/proxyrack/control/data/network/IPInfoRetrofit.kt
@@ -4,6 +4,11 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
+import java.net.Proxy
+import java.net.ProxySelector
+import java.net.SocketAddress
+import java.net.URI
 
 object IPInfoRetrofitInstance {
     private const val BASE_URL = "https://check.ragpets.com/"
@@ -13,6 +18,23 @@ object IPInfoRetrofitInstance {
     }
 
     private val httpClient = OkHttpClient.Builder()
+        // https://github.com/square/okhttp/issues/6877#issuecomment-1438554879
+        // Add our own proxy selector to guard against the case that the user has invalid
+        // system proxy settings. If the system settings are invalid we fallback to using
+        // no proxy for the request.
+        .proxySelector(object : ProxySelector() {
+            override fun select(uri: URI?): List<Proxy> {
+                return try {
+                    getDefault().select(uri)
+                } catch (e: Exception) {
+                    listOf(Proxy.NO_PROXY)
+                }
+            }
+
+            override fun connectFailed(uri: URI?, sa: SocketAddress?, ioe: IOException?) {
+                getDefault().connectFailed(uri, sa, ioe)
+            }
+        })
         .addInterceptor(loggingInterceptor)
         .build()
 
